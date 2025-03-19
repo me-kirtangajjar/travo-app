3a6ad48b1fc504d7efdf8a3ee6d0e24d3227d040
Kirtan Gajjar <mekirtangajjar@gmail.com>
2025-03-19 22:11:47 +0530

    fix: cors error

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
