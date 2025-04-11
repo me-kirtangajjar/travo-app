01e15d30b9fcd427d4c5e52bd85fa4c26d555004
Kirtan Gajjar <mekirtangajjar@gmail.com>
2025-04-11 12:51:24 +0530

    feat: add itinerary model and routes for creating and fetching itineraries

---
+++ models/itinerary.model.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const itinerarySchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = model("Itinerary", itinerarySchema);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/user.model");
-const jwt = require("jsonwebtoken");
+const Itinerary = require("../models/itinerary.model");
 
 router.post("/register", async (req, res) => {
   try {
@@ -33,11 +33,54 @@ router.get("/login", async (req, res) => {
       return res.status(404).json({ status: false, message: "User not found" });
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    return res.status(200).json({
+      status: true,
+      message: "User logged in successfully",
+      userId: user._id,
+    });
+  } catch (error) {
+    return res.status(500).json({ status: false, message: error.message });
+  }
+});
 
-    return res
-      .status(200)
-      .json({ status: true, message: "User logged in successfully", token });
+router.post("/itineraries", async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId);
+
+    if (!user) {
+      return res.status(404).json({ status: false, message: "User not found" });
+    }
+
+    const itinerary = {
+      userId: req.body.userId,
+      title: req.body.title,
+      description: req.body.description,
+    };
+    const newItinerary = await Itinerary.create(itinerary);
+
+    return res.status(200).json({
+      status: true,
+      message: "Itinerary saved successfully",
+      data: newItinerary,
+    });
+  } catch (error) {
+    return res.status(500).json({ status: false, message: error.message });
+  }
+});
+
+router.get("/itineraries", async (req, res) => {
+  try {
+    const user = await Itinerary.find({ userId: req.body.userId });
+
+    if (!user) {
+      return res.status(404).json({ status: false, message: "User not found" });
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: "Itineraries fetched successfully",
+      data: user,
+    });
   } catch (error) {
     return res.status(500).json({ status: false, message: error.message });
   }
